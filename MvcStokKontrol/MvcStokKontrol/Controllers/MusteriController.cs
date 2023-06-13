84619b355a7fb0ac610e94409a5a3210f0b2e538
EfeTolga1 <efetol@gmail.com>
2023-06-14 01:56:49 +0300

    Mvc Proje
    mvc,entityframework Linq,MsSql,crud işlemleri,validation kontolleri,dropdownlist,Alert,Popup Modal,JQuery data table (paging & search) ile stok kontrolü ve müşteriye satış yapabilen mvc projesi

---
+++ MvcStokKontrol/MvcStokKontrol/Controllers/MusteriController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStokKontrol.Models.Entity;
+
+namespace MvcStokKontrol.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(string p)
+        {
+            //var degerler = db.TblMusteriler.ToList();
+            //return View(degerler);
+            var degerler = from d in db.TblMusteriler select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.MusteriAd.Contains(p));
+            }
+            return View(degerler.ToList());
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TblMusteriler p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TblMusteriler.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Sil(int id)
+        {
+            var mst = db.TblMusteriler.Find(id);
+            db.TblMusteriler.Remove(mst);
+            db.SaveChanges();
+             return RedirectToAction("Index");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TblMusteriler.Find(id);
+            return View("MusteriGetir", mus);
+        }
+        public ActionResult Guncelle(TblMusteriler p1)
+        {
+            var musteri = db.TblMusteriler.Find(p1.MusteriID);
+            musteri.MusteriAd = p1.MusteriAd;
+            musteri.MusteriSoyad = p1.MusteriSoyad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcStokKontrol/MvcStokKontrol/Controllers/UrunController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStokKontrol.Models.Entity;
+
+namespace MvcStokKontrol.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblUrunler.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TblKategoriler.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KategoriAd,
+                                                 Value = i.KategoriID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TblUrunler p1)
+        {
+            var ktg = db.TblKategoriler.Where(m => m.KategoriID == p1.TblKategoriler.KategoriID).FirstOrDefault();
+            p1.TblKategoriler = ktg;
+            
+            db.TblUrunler.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var urun = db.TblUrunler.Find(id);
+            db.TblUrunler.Remove(urun);
+            db.SaveChanges(); 
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> degerler = (from i in db.TblKategoriler.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KategoriAd,
+                                                 Value = i.KategoriID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            
+            var urun = db.TblUrunler.Find(id);
+            return View("UrunGetir", urun);
+        }
+        public ActionResult Guncelle(TblUrunler p)
+        {
+            var urun = db.TblUrunler.Find(p.UrunID);
+            urun.UrunAdi = p.UrunAdi;
+            urun.Marka = p.Marka;
+            urun.Stok = p.Stok;
+            urun.Fiyat = p.Fiyat;
+            //urun.UrunKategori = p.UrunKategori;
+            var ktg = db.TblKategoriler.Where(m => m.KategoriID == p.TblKategoriler.KategoriID).FirstOrDefault();
+            urun.UrunKategori = ktg.KategoriID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
